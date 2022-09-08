fa450b76543470d134d6386043e7c4fef9d0396b
johnajiboy <johnajiboye53@gmail.com>
2022-09-08 15:15:38 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdioh>
+
+/**
+ * main - Printing
+ * 
+ * Return: Always 0 (Succesful)
+ */
+int maain(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	  return (0);
+}
