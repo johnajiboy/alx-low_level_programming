4e505547805fe520bf833d9d8982f8f35cb06b52
johnajiboy <johnajiboye53@gmail.com>
2022-09-09 04:31:05 -0700

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,34 @@
-#!/bin/bash
-#include <stdioh>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: Always 0
- */
-int maain(void)
+* main - print the string in the put function
+*
+*
+Description: using the main function
+*This program prints "Programming  is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	  return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
+-c
+
+
+
+
+
+
+
+
+
+
+
+
+[Bkjhgtyhjkkk:x
+
+
+
+
+
