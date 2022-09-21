04ff00d4e54af50b35e143370c32aab00fc4b204
johnajiboy <johnajiboye53@gmail.com>
2022-09-21 11:37:48 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,18 @@
-
-
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * strcat - Concatenates the string pointed to by @src, including the terminat                                                       
- *          null byte, to the end of the string pointed to by @dest.                                                                 
- * @dest: A pointer to the string to be concatenated upon.                                                                           
- * @src: The source string to be appended to @dest.                                            
- *                                                                                             * Return: A pointer to the destination string @dest.                                                                                
- */                                                                                                                                  
-char *strcat(char *dest, const char *src)                                                                                            
+#include "main.h"                                                                                                                                                              
+/**                                                                                              * strcat - Concatenates the string pointed to by @src to                                        * the end of the string pointed to by @dest                                                     * @dest: Strings that will be appended                                  
+ * @src: String to be concatenated upon                     
+ *                                               
+ * Return: returns pointer to @dest.                                                             */                                                                                                                                  
+char *_strcat(char *dest,char *src)                                                                                           
 {                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
-                                                                                                                                     
+        int index = 0, dest_len = 0;                                                                                                                                                                                                                                     
         while (dest[index++])                                                                                                        
                 dest_len++;                                                                                                          
-                                                                                                                                 
+                                                                                 
         for (index = 0; src[index]; index++)                                                                                         
                 dest[dest_len++] = src[index];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
+                                                                                               
+        return (dest);                                                                        
 }
 
 
