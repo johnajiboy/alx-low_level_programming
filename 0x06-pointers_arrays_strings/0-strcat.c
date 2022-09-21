7605d7b015447cb4e5647de010e8e5e4ef6dbf2d
johnajiboy <johnajiboye53@gmail.com>
2022-09-21 11:06:13 -0700

    streat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,13 @@
 
+
 #include "main.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
  * strcat - Concatenates the string pointed to by @src, including the terminat                                                       
  *          null byte, to the end of the string pointed to by @dest.                                                                 
  * @dest: A pointer to the string to be concatenated upon.                                                                           
- * @src: The source string to be appended to @dest.                                                                                  
- *                                                                                                                                   
- * Return: A pointer to the destination string @dest.                                                                                
+ * @src: The source string to be appended to @dest.                                            
+ *                                                                                             * Return: A pointer to the destination string @dest.                                                                                
  */                                                                                                                                  
 char *strcat(char *dest, const char *src)                                                                                            
 {                                                                                                                                    
@@ -15,7 +15,7 @@ char *strcat(char *dest, const char *src)
                                                                                                                                      
         while (dest[index++])                                                                                                        
                 dest_len++;                                                                                                          
-                                                                                                                                     
+                                                                                                                                 
         for (index = 0; src[index]; index++)                                                                                         
                 dest[dest_len++] = src[index];                                                                                       
                                                                                                                                      
@@ -23,3 +23,7 @@ char *strcat(char *dest, const char *src)
 }
 
 
+
+ 
+                                                                                                                                                                                                                                   
+       
